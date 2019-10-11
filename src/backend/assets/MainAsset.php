5aa56d01e207f079c4bc7f187f479ca7778e0b5e
Albert <albert.gee.ca@gmail.com>
2019-10-11 16:24:32 -0700

    Adds script.js to MainAsset on backend.

--- src/backend/assets/MainAsset.php
+++ src/backend/assets/MainAsset.php
@@ -1,22 +1,25 @@
 <?php
 namespace albertgeeca\language_manager\src\backend\assets;
 
-use rmrevin\yii\fontawesome\FontAwesome;
-use yii\web\AssetBundle;
-
 /**
  * Class MainAsset represents a bundle of assets which are used in each view file
  * @package albertgeeca\language_manager\src\backend\assets
  * @author Albert Gee
  */
-class MainAsset extends AssetBundle
+class MainAsset extends \yii\web\AssetBundle
 {
 
-    public $sourcePath = '@vendor/albertgeeca/yii2-language-manager/backend/web';
+    public $sourcePath = '@vendor/albertgeeca/yii2-language-manager/src/backend/web';
+
+    public $css = [];
+
+    public $js = [
+        'js/script.js'
+    ];
 
     public $depends = [
         \yii\bootstrap\BootstrapAsset::class,
-        FontAwesome::
+        \yii\web\JqueryAsset::class,
     ];
 
 }
---
+++ src/backend/web/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    const languageIDs = $('#language-list').yiiGridView('getSelectedRows');
+
+});
+
+
