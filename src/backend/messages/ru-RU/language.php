b59ecba5f116d5bf9f594379c68cc1ba8f9db02a
Albert <albert.gee.ca@gmail.com>
2019-10-12 23:01:27 -0700

    Adds translations.

--- src/backend/messages/ru-RU/language.php
+++ src/backend/messages/ru-RU/language.php
@@ -3,8 +3,10 @@
 return [
     'All' => 'Все',
     'Are you sure? You will not be able to revert this!' => 'Вы уверены? Вы не сможете восстановить удалённые данные',
+    'Default language can not be archived' => 'Язык по-умолчанию не может быть архивирован',
+    'Default language can not be deleted' => 'Язык по-умолчанию не может быть удалён',
     'Default language not found' => 'Язык, используемый по умолчанию, не найден',
     'Language with such ID not found' => 'Язык с указанным ID не найден',
     'The language has been successfully saved' => 'Язык успешно сохранён',
-    'This language is default already' => 'Этот язык уже является языком по-умолчанию'
+    'This language is default already' => 'Этот язык уже является языком по-умолчанию',
 ];
